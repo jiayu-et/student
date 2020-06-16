abe97d74cced75fc3adaf49397388df45dd4bafd
jiayu_01 <1847438558_01@qq.com>
2020-06-16 10:38:02 +0800

    在家开发请假和画图的剩余功能

---
+++ Paint.java
@@ -0,0 +1,11 @@
+/*
+* @Author: Marte
+* @Date:   2020-06-16 10:35:50
+* @Last Modified by:   Marte
+* @Last Modified time: 2020-06-16 10:37:04
+*/
+
+public class java {
+    //在家开发的画图的剩余功能
+    private String  color;
+}
