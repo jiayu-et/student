dc3219165c806ef30705273d0a42bcaa5406eae1
jiayu_01 <1847438558@qq.com>
2020-06-16 10:32:47 +0800

    在公司开发一部分画图功能

---
+++ Paint.java
@@ -0,0 +1,13 @@
+/*
+* @Author: Marte
+* @Date:   2020-06-16 10:30:44
+* @Last Modified by:   Marte
+* @Last Modified time: 2020-06-16 10:31:53
+*/
+
+public class java {
+    //在公式开发的内容
+    public static void main(String[] args) {
+
+    }
+}
